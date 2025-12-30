0fbd32b8e31bd0b3d2b777b5308eeeebe31481c9
Shreeram Modi <smodi@smodi.net>
2025-12-29 23:49:59 -0800

    Debate Rankings
    Implements Glicko rankings for the Lincoln Douglas debate circuit

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "Python development environment for computing ratings";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in
+      {
+        devShell =
+          with pkgs;
+          mkShell {
+            packages = [
+              black
+              python313
+            ];
+
+            buildInputs = [
+              python313
+              python313Packages.pip
+              python313Packages.virtualenv
+            ];
+
+            shellHook = ''
+              # Create a virtual environment if it doesn't exist
+              if [ ! -d .venv ]; then
+                echo "Creating virtual environment..."
+                python -m venv .venv
+              fi
+
+              # Activate the virtual environment
+              source .venv/bin/activate
+
+              # Install elote if not already installed
+              if ! python -c "import elote" 2>/dev/null; then
+                echo "Installing elote..."
+                pip install elote
+              fi
+            '';
+          };
+      }
+    );
+}
